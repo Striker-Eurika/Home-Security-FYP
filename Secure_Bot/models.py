a219dc917e1e55d3efb66a9586b0446d6ba978f2
ingrid <ingrid.melin.00@gmail.com>
2024-04-02 11:52:13 +0100

    Adding media to settings & creating alert model

--- Secure_Bot/models.py
+++ Secure_Bot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Alert(models.Model):
+    photo = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.photo.url
